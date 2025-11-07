71e566074ce3ff3366bd7a410e1f8d2e517c0bd6
Sama Mostafa <231562182+SamaMostafa-Programmer@users.noreply.github.com>
2025-11-07 22:10:11 +0200

    Update 2-lazy_paginate.py

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -1,1 +1,23 @@
+#!/usr/bin/python3
+import seed
 
+
+def paginate_users(page_size, offset):
+    """Fetches a specific page from the database."""
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+
+
+def lazy_pagination(page_size):
+    """Generator that yields each page lazily."""
+    offset = 0
+    while True:
+        page = paginate_users(page_size, offset)
+        if not page:
+            break
+        yield page
+        offset += page_size
