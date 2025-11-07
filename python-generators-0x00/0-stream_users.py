e461c2b8d767eda051ab09a63c2e29eb20e86a0b
Sama Mostafa <231562182+SamaMostafa-Programmer@users.noreply.github.com>
2025-11-07 22:08:55 +0200

    Update 0-stream_users.py

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,1 +1,15 @@
+#!/usr/bin/python3
+import seed
 
+
+def stream_users():
+    """Generator that yields rows one by one from user_data."""
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data;")
+
+    for row in cursor:
+        yield row  # yields one dictionary per user
+
+    cursor.close()
+    connection.close()
