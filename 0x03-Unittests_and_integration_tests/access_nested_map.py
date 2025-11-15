682bcedae5a16600cfff9ff637f6607cc23c2ab0
Sama Mostafa <samam6778@gmail.com>
2025-11-15 18:36:26 +0200

    update unit test for access_nested_map

--- 0x03-Unittests_and_integration_tests/access_nested_map.py
+++ 0x03-Unittests_and_integration_tests/access_nested_map.py
@@ -1,7 +1,9 @@
 def access_nested_map(nested_map : dict , path : tuple) -> int :
     current = nested_map
     for key in path:
-        if key in current:
+        if not isinstance(current, dict) or key not in current:
+            raise KeyError(key)
+        elif key in current:
             current = current[key]
         else:
             raise KeyError(key)
--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,16 +2,26 @@
 import unittest
 from access_nested_map import access_nested_map
 from parameterized import parameterized
+from contextlib import contextmanager
 
 class TestAccessNestedMap(unittest.TestCase):
     
     @parameterized.expand([
-        ({"a": 1} , ("a",) , 1),
+        ({"a": 1} ,  ("a",) , 1),
         ({"a": {"b": 2}} , ("a",) , {"b": 2}),
         ({"a": {"b": 2}} , ("a","b") , 2 ),
     ])
     def test_access_nested_map(self , nested_map , path , expected):
         self.assertEqual(access_nested_map(nested_map , path) , expected)
+
+    @parameterized.expand([
+        ({} , ("a",)),
+        ({"a": 1} , ("a","b")),
+    ])
+    def test_access_nested_map_exception(self , nested_map , path):
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map , path)
+        self.assertEqual(str(cm.exception), f"'{path[-1]}'")
 
 if __name__ == "__main__" :
     unittest.main()
