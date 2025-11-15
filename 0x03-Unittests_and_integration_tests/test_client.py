35c2542d2c99d7680807b88d620a65767541f958
Sama Mostafa <samam6778@gmail.com>
2025-11-15 23:28:11 +0200

    update

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -22,7 +22,9 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org_name)
         result = client.org
         self.assertEqual(result, {"login": org_name})
-        mock_get_json.assert_called_once()
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
 
 
 class TestGithubOrgClientProperty(unittest.TestCase):
