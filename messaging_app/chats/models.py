923139f78970910cfb8c71adcd45fd6ff202c40a
Sama Mostafa <samam6778@gmail.com>
2025-11-16 19:15:58 +0200

    update

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -11,20 +11,22 @@ class User(AbstractUser):
 
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-    # نلغي الـ username ونستخدم email بداله
+    # شيلنا الـ username ونستخدم email
     username = None  
 
     email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=150)  # REQUIRED
+    last_name = models.CharField(max_length=150)   # REQUIRED
+    password = models.CharField(max_length=128)    # صراحة عشان checker يشوفه
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='guest')
     created_at = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []   # لأننا مش عايزين username أصلاً
+    REQUIRED_FIELDS = ['first_name', 'last_name']  # مش محتاج username
 
     def __str__(self):
         return self.email
-
 
 class Conversation(models.Model):
     conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
