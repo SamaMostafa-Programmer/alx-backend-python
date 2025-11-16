2ae092fd097df1cccade7b067172877d64d3ea0e
Sama Mostafa <samam6778@gmail.com>
2025-11-16 23:47:23 +0200

    Update and rename utils_mock.py to utils.py

--- 0x03-Unittests_and_integration_tests/utils_mock.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Mocking helper for get_json function"""
-
-import requests
-
-def get_json(url):
-    """Fetch JSON data from a URL"""
-    response = requests.get(url)
-    return response.json()
