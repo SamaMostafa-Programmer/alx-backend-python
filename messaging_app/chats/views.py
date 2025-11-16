0a88204986a841f3499eabbddcf10d0d134cce5c
Sama Mostafa <samam6778@gmail.com>
2025-11-16 19:24:59 +0200

    update

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,15 +1,24 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    filter_backends = [filters.OrderingFilter]
-    ordering = ['created_at']  # مثال على filter
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    filter_backends = [filters.OrderingFilter]
-    ordering = ['sent_at']
+
+    def create(self, request, *args, **kwargs):
+        """Override create to return custom response with status"""
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,  # <-- هنا استخدمنا status
+            headers=headers
+        )
