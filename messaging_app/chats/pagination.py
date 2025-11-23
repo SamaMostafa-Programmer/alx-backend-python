0f90d6afceaf8a9f221171996d906647318e7297
Sama Mostafa <samam6778@gmail.com>
2025-11-23 18:29:45 +0200

    Rename pagination class and implement custom response
    Renamed pagination class and added custom response format.

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,7 +1,13 @@
-# chats/pagination.py
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
-class TwentyPerPagePagination(PageNumberPagination):
+class MessagePagination(PageNumberPagination):
     page_size = 20
-    page_size_query_param = 'page_size'  # optional override by client
-    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            "count": self.page.paginator.count,  # REQUIRED
+            "next": self.get_next_link(),
+            "previous": self.get_previous_link(),
+            "results": data
+        })
