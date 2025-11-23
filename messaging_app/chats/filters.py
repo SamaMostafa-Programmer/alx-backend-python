a2cba414f51071397a9097a54ba557d7433a1967
Sama Mostafa <samam6778@gmail.com>
2025-11-23 18:30:08 +0200

    Refactor MessageFilter to include user and date filters

--- messaging_app/chats/filters.py
+++ messaging_app/chats/filters.py
@@ -1,19 +1,11 @@
-# chats/filters.py
 import django_filters
-from .models import Message, Conversation
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from .models import Message
 
 class MessageFilter(django_filters.FilterSet):
-    # Filter messages by a participant id or username (messages from conversations that include a user)
-    participant = django_filters.BaseInFilter(field_name='conversation__participants__id', lookup_expr='exact')
-    participant_username = django_filters.CharFilter(field_name='conversation__participants__username', lookup_expr='iexact')
-
-    # created_at range (expects ISO datetimes or dates): ?created_after=YYYY-MM-DD&created_before=YYYY-MM-DD
-    created_after = django_filters.IsoDateTimeFilter(field_name='created_at', lookup_expr='gte')
-    created_before = django_filters.IsoDateTimeFilter(field_name='created_at', lookup_expr='lte')
+    user = django_filters.NumberFilter(field_name="sender__id")
+    date_after = django_filters.DateTimeFilter(field_name="timestamp", lookup_expr="gte")
+    date_before = django_filters.DateTimeFilter(field_name="timestamp", lookup_expr="lte")
 
     class Meta:
         model = Message
-        fields = ['participant', 'participant_username', 'created_after', 'created_before']
+        fields = ["user", "date_after", "date_before"]
