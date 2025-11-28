786048364c58a61754519697c9aa3128e8a0f2a8
Sama Mostafa <samam6778@gmail.com>
2025-11-28 20:47:33 +0200

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -40,3 +40,18 @@ def unread_inbox(request):
     )
 
     return render(request, "unread.html", {"messages": unread_messages})
+
+# chats/views.py
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    @method_decorator(cache_page(60))  # cache for 60 seconds
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
