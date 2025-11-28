db35248b2623ffc9f53ea2af4666c6098b2ad9db
Sama Mostafa <samam6778@gmail.com>
2025-11-28 20:43:45 +0200

    Update views.py

--- Django-signals_orm-0x04/chats/views.py
+++ Django-signals_orm-0x04/chats/views.py
@@ -1,10 +1,14 @@
+# chats/views.py
 from django.views.decorators.cache import cache_page
-from django.shortcuts import render
-from messaging.models import Message
+from django.utils.decorators import method_decorator
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
-@cache_page(60)  # 60 seconds cache
-def conversation_messages(request, conversation_id):
-    messages = Message.objects.filter(receiver=request.user)\
-        .select_related("sender", "parent_message")\
-        .prefetch_related("replies")
-    return render(request, "messages.html", {"messages": messages})
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    @method_decorator(cache_page(60))  # cache for 60 seconds
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
