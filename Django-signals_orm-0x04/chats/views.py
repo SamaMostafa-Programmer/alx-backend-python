b9b5f02e0644ece6fcd8d65e9284297f1be96e63
Sama Mostafa <samam6778@gmail.com>
2025-11-28 15:41:03 +0200

    Update

--- Django-signals_orm-0x04/chats/views.py
+++ Django-signals_orm-0x04/chats/views.py
@@ -6,5 +6,7 @@ from .models import Message
 
 @cache_page(60)  # 60 seconds cache
 def conversation_messages(request, conversation_id):
-    messages = Message.objects.filter(receiver=request.user).select_related("sender", "parent_message").prefetch_related("replies")
+    messages = Message.objects.filter(receiver=request.user)\
+        .select_related("sender", "parent_message")\
+        .prefetch_related("replies")
     return render(request, "messages.html", {"messages": messages})
