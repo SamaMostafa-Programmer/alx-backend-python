920a6ad70ce68b372829ec38b75d9784ad5ba2be
Sama Mostafa <samam6778@gmail.com>
2025-11-28 15:29:12 +0200

    Update

--- Django-signals_orm-0x04/messaging_app/chats/views.py
+++ Django-signals_orm-0x04/messaging_app/chats/views.py
@@ -1,7 +1,10 @@
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.shortcuts import render
+from .models import Message
 
-@cache_page(60)
-def conversation_messages(request, conv_id):
-    messages = Message.objects.filter(conversation_id=conv_id)
+@cache_page(60)  # 60 seconds cache
+def conversation_messages(request, conversation_id):
+    messages = Message.objects.filter(receiver=request.user).select_related("sender", "parent_message").prefetch_related("replies")
     return render(request, "messages.html", {"messages": messages})
